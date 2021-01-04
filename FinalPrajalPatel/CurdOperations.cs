997a698cc554cd3909fca49b87fc3856199fb57f
paprajal <patelprajal96@gmail.com>
2021-01-04 14:54:02 -0500

    Add project files.

---
+++ FinalPrajalPatel/CurdOperations.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalPrajalPatel
+{
+    class CurdOperations
+    {
+        public static void GetAllCities()
+        {
+            using (var context = new worldDBEntities())
+            {
+                var cities = context.Cities.ToList();
+
+
+                foreach (var city in cities)
+                {
+                    var capAsk = "";
+
+                    if(city.IsCapital == true)
+                    {
+                        capAsk = "Yes";
+                    }
+
+                    Console.WriteLine($"{city.CityID, -5} {city.CityName, -20} {capAsk, -20} {city.Country.CountryName, -20} {city.Country.Continent.ContinentName, -20} {city.Population, -5}");
+                }
+            }
+        }
+        public static void GetAllCountries()
+        {
+            using (var context = new worldDBEntities())
+            {
+                var countries = context.Countries.ToList();
+                
+
+                foreach (var country in countries)
+                {
+                    var showCapital = (from s in context.Cities
+                                       where s.CountryID == country.CountryID && s.IsCapital == true
+                                       select s.CityName).FirstOrDefault();
+                    Console.WriteLine($"{country.CountryID,-10} {country.CountryName,-20}  {showCapital, -20}{country.Continent.ContinentName, -20} {country.Language, -20} {country.Currency,-20}");
+                }
+            }
+
+        }
+        public static void GetAllContinents()
+        {
+            using (var context = new worldDBEntities())
+            {
+                var contients = context.Continents.ToList();
+
+                foreach (var contient in contients)
+                {
+                    Console.WriteLine($"{contient.ContinentID, 10} {contient.ContinentName,-10}");
+                }
+            }
+
+        }
+        public static void GetAllCountry()
+        {
+            using (var context = new worldDBEntities())
+            {
+                var countries = context.Countries.ToList();
+
+                foreach (var country in countries)
+                {
+                    Console.WriteLine($"{country.CountryID, 10} {country.CountryName, -10}");
+                }
+            }
+
+        } 
+
+        public static void GetAllCity()
+        {
+            using (var context = new worldDBEntities())
+            {
+                var cities = context.Cities.ToList();
+
+                foreach (var city in cities)
+                {
+                    Console.WriteLine($"{city.CityID,10} {city.CityName,-10}");
+                }
+            }
+        }
+
+
+        public static void AddNewCountry(string code, string cn,
+            string ln, string cu, int bt)
+        {
+            using (var context = new worldDBEntities())
+            {
+                
+                Country con = new Country();
+                
+                con.CountryCode = code;
+                con.CountryName = cn;
+                con.Language = ln;
+                con.Currency = cu;
+                con.ContinentID = bt;
+                
+
+                context.Countries.Add(con);
+                context.SaveChanges();
+
+            }
+
+        }
+        public static void AddNewCity(string cn, string capp,
+            long pop, int cou)
+        {
+            bool cpp;
+            if (capp == "Y" || capp == "y")
+            {
+                cpp = true;
+            }
+            else
+            {
+                cpp = false;
+            }
+            using (var context = new worldDBEntities())
+            {
+                
+
+                City ci = new City();
+
+                ci.CityName = cn;
+                ci.IsCapital = cpp;
+                ci.Population = pop;
+                ci.CountryID = cou;
+
+
+                context.Cities.Add(ci);
+                context.SaveChanges();
+            }
+
+        }
+         public static void GetCityByID(int id)
+        {
+            using (var context = new worldDBEntities())
+            {
+                var show = (from a in context.Cities
+                              where a.CityID == id
+                              select a).FirstOrDefault();
+
+                var capAsk = "";
+
+                if (show.IsCapital == true)
+                {
+                    capAsk = "Yes";
+                }
+                else
+                {
+                    capAsk = "No";
+                }
+                Console.WriteLine("\nCityID\t CityName\t IsCapital\t Population\t CountryID \n");
+                Console.WriteLine($"{show.CityID,5}  {show.CityName, -20}  {capAsk,-10} {show.Population,-20} {show.CountryID, -10} ");
+            }
+        }
+        public static void UpdateCity(int id, string name, string cap, long calc, int uni)
+        {
+            bool cpp;
+            if (cap == "Y" || cap == "y")
+            {
+                cpp = true;
+            }
+            else
+            {
+                cpp = false;
+            }
+            using (var context = new worldDBEntities())
+            {
+                City cy = context.Cities.Find(id);
+
+                cy.CityName = name;
+                cy.IsCapital = cpp;
+                cy.Population = calc;
+                cy.CountryID = uni;
+                
+
+                context.SaveChanges();
+            }
+
+        }
+
+    }
+}
---
+++ FinalPrajalPatel/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalPrajalPatel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\tFINAL EXAM  Prajal Patel");
+
+            do
+            {
+                int input = DisplayMenu();
+
+                    switch (input)
+                    {
+                        case 1:
+                            Console.WriteLine("\n================================================================================================================\n");
+                        Console.WriteLine("\nCityID\t CityName\t IsCapital\t\t CountryName\t ContinentName\t\t Population \n");
+                        
+                        CurdOperations.GetAllCities();
+                
+                        break;
+
+                       case 2:
+                           Console.WriteLine("\n================================================================================================================\n");
+                        Console.WriteLine("\nCountryID\t CountryName\t Capital\t ContinentName\t\t Language\t\t Currency \n");
+                        CurdOperations.GetAllCountries();
+
+                       
+                        break;
+
+                       case 3:
+                           Console.WriteLine("\n================================================================================================================\n"); 
+                        Console.Write("Enter Country Code(2 Letters): ");
+                        string code = Console.ReadLine();
+
+                        Console.Write("Enter Country Name: ");
+                        string cn = Console.ReadLine();
+
+                        Console.Write("Enter Language: ");
+                        string ln = Console.ReadLine();
+
+                        Console.Write("Enter Currency: ");
+                        string cu = Console.ReadLine();
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("ContinentId  ContinentName");
+                        Console.WriteLine("\n");
+                        CurdOperations.GetAllContinents();
+                        Console.WriteLine("\n\n");
+                        Console.Write("Enter Continent ID: ");
+                        int bt = int.Parse(Console.ReadLine());
+                        Console.WriteLine("\n");
+
+
+                        CurdOperations.AddNewCountry(code, cn, ln, cu, bt);
+                        Console.Write("New Country is added: ");
+                        break;
+
+
+                         case 4:
+                        Console.WriteLine("\n================================================================================================================\n");
+                        
+
+                        Console.Write("Enter City Name: ");
+                         cn = Console.ReadLine();
+
+                       Console.Write("Enter Is Capital(y/n): ");
+                       string capp = Console.ReadLine();
+
+                        Console.Write("Enter Population of city: ");
+                        long pop = long.Parse(Console.ReadLine());
+
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("CountryId CountryName");
+                        Console.WriteLine("\n");
+                        CurdOperations.GetAllCountry();
+
+                        Console.WriteLine("\n\n");
+                        Console.Write("Enter Country Id: ");
+                        int cou = int.Parse(Console.ReadLine());
+                        Console.WriteLine("\n");
+
+
+
+                        CurdOperations.AddNewCity(cn, capp, pop, cou);
+                        Console.Write("New City is added: ");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("\n================================================================================================================\n");
+                        Console.WriteLine("CityId CityName");
+                        Console.WriteLine("\n");
+                        CurdOperations.GetAllCity();
+                        Console.WriteLine("\n");
+                        Console.Write("Enter city ID (which data will you want to update): ");
+                        int id = int.Parse(Console.ReadLine());
+                        Console.WriteLine("\n");
+                        Console.WriteLine("---------------------- Old Data of city ID ----------------------\n");
+                        Console.WriteLine("\n");
+                        CurdOperations.GetCityByID(id);
+                        Console.WriteLine("\n");
+                        Console.WriteLine("\n----------------------- Enter new data of city ----------------------");
+                        Console.WriteLine("\n");
+
+                        Console.Write("Enter City Name: ");
+                        string name = Console.ReadLine();
+
+                        Console.Write("Enter Capital(y/n): ");
+                       string cap =Console.ReadLine();
+
+                        Console.Write("Enter Population of city: ");
+                        long calc = long.Parse(Console.ReadLine());
+
+                        Console.WriteLine("\n");
+                        Console.WriteLine("CountryId CountryName");
+                        Console.WriteLine("\n");
+                        CurdOperations.GetAllCountry();
+                        Console.WriteLine("\n");
+
+                        Console.Write("Enter Country Id: ");
+                        int uni = int.Parse(Console.ReadLine());
+                        Console.WriteLine("\n");
+
+
+
+                        CurdOperations.UpdateCity(id, name, cap, calc, uni);
+                        Console.Write("New City is updated: ");
+                        break;
+
+                    case 6:
+                           Environment.Exit(0);
+                           break;
+
+                       default:
+                           Console.WriteLine("\n........ PLEASE ENTER VALID INPUT......");
+                            break;
+                   }
+                } while (true);
+
+
+            }
+
+        static int DisplayMenu()
+        {
+
+            Console.WriteLine("\n\n================================================================================================================\n");
+            Console.WriteLine("\t 1. Get all Cities");
+            Console.WriteLine("\t 2. Get all Countries");
+            Console.WriteLine("\t 3. Add new Country");
+            Console.WriteLine("\t 4. Add new City");
+            Console.WriteLine("\t 5. Update a city");
+            Console.WriteLine("\t 6. Exit");
+
+            Console.Write("\n\nEnter your choice: ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
